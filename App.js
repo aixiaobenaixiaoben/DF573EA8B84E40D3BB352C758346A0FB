2b6e29a0bff57a8aa647bcfa3b6835be287d355e
kevin <357620917@qq.com>
2018-05-21 17:09:02 +0800

    test antd mobile

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
-import {Button} from "antd-mobile";
 
 
 class Root extends Component{
@@ -9,7 +8,6 @@ class Root extends Component{
     return (
       <View style={{ padding: 20,}}>
         <Text>hello react</Text>
-        <Button type="primary" onClick={() => alert(1)}>hello antd</Button>
       </View>
     );
   }
